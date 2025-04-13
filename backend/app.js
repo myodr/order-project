fc34940ca2cd18fa94a86e440dc0e540a050ff63
kgs-ext <kgssks@gmail.com>
2025-04-13 15:45:02 +0900

    local test

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// const {getEvent}  = require('lambda/getEvent');
+const getEventPage = require('./lambda/getEventPage');
+
+// 미들웨어: JSON 바디 파싱
+app.use(express.json());
+
+// 기본 라우터
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+app.get('/getEvents/:id', async (req, res) =>  {
+    let event = {
+        pathParameters:{
+            event_id: req.params.id
+        }
+    }
+    let resp = await getEventPage.handler(event);
+    res.send(resp);
+});
+// 예시: POST 요청 처리
+app.post('/api/data', (req, res) => {
+    const data = req.body;
+    console.log('Received data:', data);
+    res.status(201).json({ message: 'Data received', data });
+});
+
+// 서버 시작
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
